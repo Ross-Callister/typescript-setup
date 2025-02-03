a68988337304f0d4d464262fae4748a23b652ac6
Ross Callister <ross.callister@outlook.com>
2025-02-03 17:50:18 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import { getRandPokemon } from "./pokemon.js";
+
+const server = fastify();
+
+server.get('/random', (req, reply) => {
+    const pokemon = getRandPokemon();
+    reply.send(pokemon);
+})
+
+server.ready().then(() => {
+    console.log("Latias best pokemon");
+})
+
+server.listen({
+    port: 9151,
+})
