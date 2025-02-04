3c0ce67144d81e730ddc03c9e536273bdc86a636
Ross Callister <ross.callister@smartsheet.com>
2025-02-04 10:36:10 +0800

    linting changes.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import { getRandPokemon } from "./pokemon.js";
 
 const server = fastify();
 
-server.get('/random', (req, reply) => {
-    const pokemon = getRandPokemon();
-    reply.send(pokemon);
-})
+server.get("/random", (req, reply) => {
+  const pokemon = getRandPokemon();
+  reply.send(pokemon);
+});
 
 server.ready().then(() => {
-    console.log("Latias best pokemon");
-})
+  console.log("Latias best pokemon");
+});
 
 server.listen({
-    port: 9151,
-})
+  port: 9151,
+});
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,43 +1,43 @@
 export const pokemon = {
-    pikachu: {
-        name: 'Pikachu',
-        type: 'Electric',
-    },
-    charmander: {
-        name: 'Charmander',
-        type: 'Fire',
-    },
-    squirtle: {
-        name: 'Squirtle',
-        type: 'Water',
-    },
-    bulbasaur: {
-        name: 'Bulbasaur',
-        type: 'Grass',
-    },
-    pidgey: {
-        name: 'Pidgey',
-        type: 'Flying',
-    },
-    jigglypuff: {
-        name: 'Jigglypuff',
-        type: 'Fairy',
-    },
-    meowth: {
-        name: 'Meowth',
-        type: 'Normal',
-    },
-    abra: {
-        name: 'Abra',
-        type: 'Psychic',
-    },
-}
+  pikachu: {
+    name: "Pikachu",
+    type: "Electric",
+  },
+  charmander: {
+    name: "Charmander",
+    type: "Fire",
+  },
+  squirtle: {
+    name: "Squirtle",
+    type: "Water",
+  },
+  bulbasaur: {
+    name: "Bulbasaur",
+    type: "Grass",
+  },
+  pidgey: {
+    name: "Pidgey",
+    type: "Flying",
+  },
+  jigglypuff: {
+    name: "Jigglypuff",
+    type: "Fairy",
+  },
+  meowth: {
+    name: "Meowth",
+    type: "Normal",
+  },
+  abra: {
+    name: "Abra",
+    type: "Psychic",
+  },
+};
 
 export function getRandPokemon() {
-    const keys = Object.keys(pokemon);
-    return pokemon[keys[Math.floor(Math.random() * keys.length)]];
+  const keys = Object.keys(pokemon);
+  return pokemon[keys[Math.floor(Math.random() * keys.length)]];
 }
 
 export function getPokemonByName(name) {
-    return pokemon[name];
+  return pokemon[name];
 }
